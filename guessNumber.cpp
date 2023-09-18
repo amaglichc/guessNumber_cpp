0ca24ea7b3f9e9c44962b9f01d2a6615501235b5
Mike <amaglichc@gmail.com>
2023-09-18 13:16:51 +0300

    first version of the game

---
+++ guessNumber.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int rand_res =1 + rand() % 15;
+	int per_res;
+	cout << "try to guess the number: ";
+	cin >> per_res;
+	if (per_res == rand_res) {
+		cout << "you guessed the number!!!";
+	}
+	if  (per_res != rand_res) {
+		cout << "you didnt guessed the number :( the number be  "<<rand_res;
+
+	}
+}
+ 
