eafe63dbbd242bd86fbd7b768cb4f55c6310c69c
Mike <amaglichc@gmail.com>
2023-09-18 14:27:05 +0300

    ver2, now game dont end if you didnt guess the nuber

---
+++ guessTheNumber.ver2/guessTheNumber.ver2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int rand_res = 1 + rand() % 20;
+	int per_res;
+	bool stop_game = false;
+
+	do {
+		cout << "try to guess the number: ";
+		cin >> per_res;
+		if (per_res != rand_res) {
+			cout << "you didnt guessed the number :("<<endl;
+
+		}
+		else {
+			cout << "you guessed the number!!!";
+			stop_game = true;
+		}
+	} while (!stop_game);
+}
